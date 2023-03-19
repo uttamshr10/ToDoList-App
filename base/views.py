665a0006571b0b3c1e2749cb08b4bcc31b8738de
Uttam <utam.shrestha65@gmail.com>
2023-03-19 18:48:48 +0545

    searching logic added

--- base/views.py
+++ base/views.py
@@ -1,9 +1,11 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.shortcuts import render, redirect
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView, FormView
 from base import models
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 
 
 class CustomLoginView(LoginView):
@@ -13,10 +15,39 @@ class CustomLoginView(LoginView):
 
     def get_success_url(self):
         return reverse_lazy('tasks')
+
+class RegisterPage(FormView):
+    template_name = 'base/register.html'
+    form_class = UserCreationForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user is not None:
+            login(self.request, user)
+        return super(RegisterPage, self).form_valid(form)
+    # with this, user will not be able to go to register page directly with link.
+    def get(self, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('tasks')
+        return super(RegisterPage, self).get(*args, **kwargs)
+
 class taskList(LoginRequiredMixin, ListView):
     model = models.Task
     context_object_name = 'tasks'
-
+    # Cannot view, edit or delete other people TODO items with get_context_data function.
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tasks'] = context['tasks'].filter(user=self.request.user)
+        context['count'] = context['tasks'].filter(complete = False).count()
+        # this is for search logic
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(
+                title__startswith=search_input) # with startswith when we search anything it will give us item that startswith what we are searching for 
+        context['search_input'] = search_input
+        return context
 
 class taskDetail(LoginRequiredMixin, DetailView):
     model = models.Task
@@ -27,13 +58,16 @@ class taskDetail(LoginRequiredMixin, DetailView):
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = models.Task
     context_object_name = 'create'
-    fields = '__all__'
+    fields = ['title', 'description', 'complete'] # with this, user cannot enter any items of other.
     success_url = reverse_lazy('tasks')
 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super(TaskCreate, self).form_valid(form)
 
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = models.Task
-    fields = '__all__'
+    fields = ['title', 'description', 'complete']
     success_url = reverse_lazy('tasks')
 
 
