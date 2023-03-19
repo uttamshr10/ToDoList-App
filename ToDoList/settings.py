4a7c19ce74a89bc3702c8e2a040af5e869568359
Uttam <utam.shrestha65@gmail.com>
2023-03-19 17:17:22 +0545

    Credentials hidden

--- ToDoList/settings.py
+++ ToDoList/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9ph1dbrdq%s$l=z&dhimcvv3opt79g23w=%70))=6yfon@s64_'
+SECRET_KEY = config('SECRET_KEY', default = '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast = bool, default = True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    host.strip()
+    for host in config('ALLOWED_HOSTS', default = '').split(',')
+]
 
 
 # Application definition
